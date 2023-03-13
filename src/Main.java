44e6f440cae2b936a1040d88d0448eff030ba546
Dmitry <once87@yandex.ru>
2023-03-13 19:57:34 +0300

    Update

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 17100;
+        int mile = 20;
+        int bonus = price / mile;
+
+        System.out.println(price / mile);
+
+
+    }
+}
